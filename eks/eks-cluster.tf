ec7e28ec856218a02e128def4daf31e91e15fda8
Ravi kumar <ravikumar.manchi@ihub-data.iiit.ac.in>
2025-11-21 16:14:35 +0530

    terraform init

---
+++ eks/eks-cluster.tf
@@ -0,0 +1,20 @@
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = var.private_subnets
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
+  ]
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.this.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "cluster_name" {
+  default = "my-eks-cluster"
+}
+
+variable "node_group_instance" {
+  default = "t3.medium"
+}
+
+variable "desired_size" {
+  default = 2
+}
+
+variable "min_size" {
+  default = 1
+}
+
+variable "max_size" {
+  default = 3
+}
